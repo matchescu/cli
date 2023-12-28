6cd9062c75743474af8db799932b91667aa3a202
Andrei Olar <andrei.olar@dpgmedia.nl>
2023-12-28 15:55:14 +0200

    feat: use universal logging settings in timer

--- src/matchescu/instrumentation/timer.py
+++ src/matchescu/instrumentation/timer.py
@@ -1,26 +1,23 @@
 import functools
-import logging
-import sys
 import time
 import datetime
 
+from matchescu.logs import get_logger
 
-def timer(text: str, log: logging.Logger = None):
-    if log is None:
-        log = logging.getLogger(timer.__name__)
-        log.setLevel(logging.INFO)
-        log.addHandler(logging.StreamHandler(stream=sys.stdout))
 
+def timer(text: str):
     def timer_decorator(f):
+
         @functools.wraps(f)
         def __wrap(*args, **kwargs):
+            log = get_logger(text)
             start = time.time()
             try:
-                log.info("starting %s", text)
                 return f(*args, **kwargs)
             finally:
                 delta = datetime.timedelta(seconds=time.time() - start)
-                log.info("%s took %s", text, delta)
+                log.info("duration=%(duration)s", {"duration": delta})
+
         return __wrap
 
     return timer_decorator
